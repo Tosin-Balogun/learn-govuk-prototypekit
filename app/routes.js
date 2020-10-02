71e4cadd6f018ae09a90ac58a9e8cc8d7ed28728
Tosin Balogun <tosin@tosins-mbp.home>
2020-10-02 16:52:28 +0100

    add allergy questions with branch parts

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,19 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+// Branching
+router.post('/question-toast-failure', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    const glutenAllergy = req.session.data['gluten-allergy']
+  
+    if (glutenAllergy === 'Yes') {
+      res.redirect('/question-toast-failure')
+    } else {
+      res.redirect('/question-toast-1')
+    }
+  })
